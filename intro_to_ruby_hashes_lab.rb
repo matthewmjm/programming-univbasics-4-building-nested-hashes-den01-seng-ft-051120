1e3a7345c1687a8feef7365e0646e1e267ace8b9
Matthew Malecki <mmalecki4@gmail.com>
2020-04-16 21:38:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,6 +31,12 @@ def monopoly_with_third_tier
       base_hash[:railroads][:pieces] = 4
     base_hash[:railroads][:rent_in_dollars] = Hash.new
       base_hash[:railroads][:rent_in_dollars][:one_piece_owned]
+      
+      base_hash[:railroads][:rent_in_dollars][:one_piece_owned]
+            
+      base_hash[:railroads][:rent_in_dollars][:one_piece_owned]
+                  
+      base_hash[:railroads][:rent_in_dollars][:one_piece_owned]
     base_hash[:railroads][:names] = Hash.new
   base_hash
 end
