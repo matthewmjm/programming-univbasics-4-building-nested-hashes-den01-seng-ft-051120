bd00df7dce55c5c71bcdb366807a00930ad57a18
Matthew Malecki <mmalecki4@gmail.com>
2020-04-16 21:48:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -66,6 +66,7 @@ def monopoly_with_fourth_tier
         base_hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
     base_hash[:railroads][:names] = Hash.new
         base_hash[:railroads][:names][:reading_railroad] = Hash.new
+                base_hash[:railroads][:names][:reading_railroad] = Hash.new
         base_hash[:railroads][:names][:pennsylvania_railroad] = Hash.new
         base_hash[:railroads][:names][:b_and_o_railroad] = Hash.new
         base_hash[:railroads][:names][:shortline_railroad] = Hash.new
