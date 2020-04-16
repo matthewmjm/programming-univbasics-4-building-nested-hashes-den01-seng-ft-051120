af8fb97242c51fe83b24370649b99a23b6b3bb33
Matthew Malecki <mmalecki4@gmail.com>
2020-04-16 21:47:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,5 +51,23 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  base_hash = Hash.new
+    base_hash[:railroads] = Hash.new
+    base_hash[:railroads][:pieces]
+      base_hash[:railroads][:pieces] = 4
+    base_hash[:railroads][:rent_in_dollars] = Hash.new
+      base_hash[:railroads][:rent_in_dollars][:one_piece_owned]
+        base_hash[:railroads][:rent_in_dollars][:one_piece_owned]  = 25
+      base_hash[:railroads][:rent_in_dollars][:two_pieces_owned]
+        base_hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+      base_hash[:railroads][:rent_in_dollars][:three_pieces_owned]
+        base_hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+      base_hash[:railroads][:rent_in_dollars][:four_pieces_owned]
+        base_hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+    base_hash[:railroads][:names] = Hash.new
+        base_hash[:railroads][:names][:reading_railroad] = Hash.new
+        base_hash[:railroads][:names][:pennsylvania_railroad] = Hash.new
+        base_hash[:railroads][:names][:b_and_o_railroad] = Hash.new
+        base_hash[:railroads][:names][:shortline_railroad] = Hash.new
+  base_hash
 end
